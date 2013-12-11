a1251a81e2c5e438d261279208abffbfa5128837
dyosukeb <t10541yt@sfc.keio.ac.jp>
2013-12-11 17:56:54 +0900

    Initial Commit

---
+++ ppp/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ppp
+//
+//  Created by 高橋洋介 on 2013/12/11.
+//  Copyright (c) 2013年 高橋洋介. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
